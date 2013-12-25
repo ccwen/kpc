ad008bc40f509708df0e808d996f70a75a54461d
yap cheah shen <yapcheahshen@gmail.com>
2013-12-25 22:46:15 +0800

    remove mediator

--- components/ksanaforge-boot/index.js
+++ components/ksanaforge-boot/index.js
@@ -1,4 +1,3 @@
 React=require('../react');
 Require=function(arg){return require("../"+arg)};
-mediator=new require("../mediator").Mediator(); //
 React.renderComponent(require("../main")(),document.getElementById("main"));
